065a615bd33ca91b07e5dbd926fb22b49aa3300b
Dylan Possamaï <dylansmb@gmail.com>
2025-11-10 18:21:54 -0800

    Fix expander smoke JSON literal

--- core/l1_expander/expander_smoke.ml
+++ core/l1_expander/expander_smoke.ml
@@ -1,14 +1,13 @@
-  open L1_expander
+open L1_expander
 
-  let () =
-    let inp =
-      if Array.length Sys.argv > 1 then Sys.argv.(1)
-      else "Before \\textbf{A \\emph{B} C} after"
-    in
-    let cfg =
-      if Array.length Sys.argv > 2 then Catalogue_loader.load Sys.argv.(2)
-      else Catalogue_loader.default
-    in
-    let expanded = Simple_expander.expand_fix_with cfg inp in
-    Printf.sprintf {|{"expanded":%S,"validators":[]}|} expanded |> print_endline
-  EOF
+let () =
+  let inp =
+    if Array.length Sys.argv > 1 then Sys.argv.(1)
+    else "Before \\textbf{A \\emph{B} C} after"
+  in
+  let cfg =
+    if Array.length Sys.argv > 2 then Catalogue_loader.load Sys.argv.(2)
+    else Catalogue_loader.default
+  in
+  let expanded = Simple_expander.expand_fix_with cfg inp in
+  Printf.sprintf {|{"expanded":%S,"validators":[]}|} expanded |> print_endline
